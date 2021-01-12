433df6b663f568f3f74f6a3aee04aaa34c72f7c9
TiokengSamuel <samueledward40@gmail.com>
2021-01-12 12:03:18 +0100

    completed server.js

--- server.js
+++ server.js
@@ -3,3 +3,11 @@ const app = express();
 
 //Creating a middleware
 const fileServerMiddleware = express.static('public');
+
+//Mounting middleware
+app.use('/', fileServerMiddleware);
+
+//Running port
+app.listen(3000, function () {
+    console.log('App started on port 3000');
+})
