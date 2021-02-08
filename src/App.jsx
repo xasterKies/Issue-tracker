55e904b3f190879982ca036acb97120e8be2981b
TiokengSamuel <samueledward40@gmail.com>
2021-02-08 18:03:32 +0100

    editing its main classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
-class HelloWorld extends React.Component {
+class IssueList extends React.Component {
     render() {
-        const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
-        const helloContinents = Array.from(continents, c => 'Hello $(c)!');
-        const message = helloContinents.join(' ');
-
-        return (
-            <div title="Outer div">
-                <h1>{message}</h1>
-            </div>
-        );
+        return(
+        <React.Fragment>
+            <h1>Issue Tracker</h1>
+            <IssueFilter/>
+            <hr/>
+            <IssueTable/>
+            <hr/>
+            <IssueAdd/>
+        </React.Fragment>
+        )
     }
 }
 
-const element = <HelloWorld/>
+class IssueFilter extends React.Component {
+    render() {
+        return (
+            <div>This is a placeholder for the issue filter</div>
+        )
+    }
+}
+
+class IssueTable extends React.Component {
+    render() {
+        return (
+            <div>This is a placeholder for the issue table</div>
+        )
+    }
+}
+
+class IssueAdd extends React.Component {
+    render() {
+        return (
+            <div>This is a placeholder for the issue add</div>
+        )
+    }
+}
+
+const element = <IssueList/>
 ReactDOM.render(element, document.getElementById('contents'));
