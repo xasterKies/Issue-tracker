fd13df67faafc0de3f7fb40721430f67eda359fb
TiokengSamuel <samueledward40@gmail.com>
2021-02-08 18:16:37 +0100

    issueRow

--- src/App.jsx
+++ src/App.jsx
@@ -32,10 +32,34 @@ class IssueTable extends React.Component {
 class IssueAdd extends React.Component {
     render() {
         return (
-            <div>This is a placeholder for the issue add</div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <IssueRow/>
+                    <IssueRow/>
+                </tbody>
+            </table>
         )
     }
 }
+
+class IssueRow extends React.Component {
+    render() {
+        const style = this.props.rowStyle;
+        return (
+            <tr>
+                <td>{this.props.issue_id}</td>
+            </tr>
+        )
+    }
+}
+
+
 
 const element = <IssueList/>
 ReactDOM.render(element, document.getElementById('contents'));
